86e22ad4bf7f0ab898b3892e3545c6b2ac0017b5
Md Samreen begum <samreenbegum5211@gmail.com>
2026-06-15 15:31:59 +0530

    Add files via upload

---
+++ elseif.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks=75;
+    if(marks>75)
+    {
+cout << "Grade A";
+    }
+    else if(marks<=75)
+    {
+cout << "Grade B ";
+    }
+    else if(marks==50)
+    {
+        cout << "Grade c";
+    }
+else{
+cout<<"Fail";
+}  
+    return 0;
+}
