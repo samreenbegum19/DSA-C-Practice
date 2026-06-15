9cb4ab534de52100a315eb3ee89a9e13db4f9b8c
Md Samreen begum <samreenbegum5211@gmail.com>
2026-06-15 15:14:50 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age=18;
+    // simple if statement
+    if(age>=18){
+        cout<<"You are an adult";
+    }
+    else{
+cout << "You are not an adult";
+    }
+    return 0;
+}
