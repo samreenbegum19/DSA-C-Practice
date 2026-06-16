c65ac7d9ca23f11bf64257b32e1915081d233f26
Md Samreen begum <samreenbegum5211@gmail.com>
2026-06-16 11:14:03 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    cout<<arr[3]<<endl;
+        cout<<arr[2]+arr[3];
+
+    return 0;
+}
