4a6c8bb2b9be61a059aaea2e5134f8ddbb91df36
Md Samreen begum <samreenbegum5211@gmail.com>
2026-06-19 18:00:29 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printName()
+{//void function which does not return anything//
+    cout<<" hey samreen!";
+}
+int main()
+{
+    printName();
+    return 0;
+}
