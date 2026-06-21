53ebc0b78073b659da9e679b3d2bfcd09a8af9d2
Md Samreen begum <samreenbegum5211@gmail.com>
2026-06-21 22:45:54 +0530

    Add files via upload

---
+++ loop1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string name="samreenbegum";
+    for(int i=name.length()-1;i>=0;i--)
+    {
+        cout<<name[i];
+    }
+    return 0;
+}
