10779236cd6583310903404aad46f1f7fc8c4f44
Md Samreen begum <samreenbegum5211@gmail.com>
2026-06-26 23:13:29 +0530

    Add files via upload

---
+++ arthmetic.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2,num3,num4;
+    cin>>num1>>num2>>num3>>num4;
+    int num5=num1+num2-num3*num4;
+    cout<<num4;
+    return 0;
+}
